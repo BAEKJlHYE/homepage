2d67b4e2c4a6b657e1b2c7baac3fe424027b09a6
jihye <bjiekie@gmail.com>
2023-01-27 17:35:54 +0900

    건물 등록 진행 중

--- src/main/java/anonymous/homepage/buld/web/BuldController.java
+++ src/main/java/anonymous/homepage/buld/web/BuldController.java
@@ -1,12 +1,15 @@
 package anonymous.homepage.buld.web;
 
+import anonymous.homepage.board.vo.BoardVO;
 import anonymous.homepage.buld.service.BuldService;
 import anonymous.homepage.buld.vo.BuldVO;
+import anonymous.homepage.cd.service.CdService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,11 +21,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BuldController {
     private final BuldService buldService;
+    private final CdService cdService;
 
     @GetMapping("/selectBuldList.do")
     public String selectBuldList(Model model) {
         model.addAttribute("buldList", buldService.selectBuldList());
         return "buld/buldList";
+    }
+
+    // 공지사항 등록 화면 이동
+    @GetMapping("/registerBuld.do")
+    public String registerBoard(Model model) {
+        // 거래유형구분코드
+        model.addAttribute("delngTySeCds", cdService.selectCdList("A01"));
+        // 계약상태구분코드
+        model.addAttribute("cntrctSttusSeCds", cdService.selectCdList("A02"));
+        // 매물구분코드
+        model.addAttribute("saleSeCds", cdService.selectCdList("A03"));
+
+        return "buld/buldRegister";
     }
 
 }
